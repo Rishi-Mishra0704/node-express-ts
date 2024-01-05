1618c5ab1730613fe858dfc2159edfc0f4c87723
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-01-05 23:13:18 +0530

    add more controller

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,12 +1,54 @@
 import { Request, Response } from "express";
-import Product from "../models/product";
+import Product, { IProduct } from "../models/product";
 
-export const getAllProducts = async(req: Request, res: Response) =>{
-    try {
-        const products = await Product.find()
-        res.status(200).json(products)
-    }
-    catch(error){
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-}
+export const getAllProducts = async (req: Request, res: Response) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const createProduct = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const newProduct: IProduct = req.body;
+    const createdProduct = await Product.create(newProduct);
+    res.status(201).json(createdProduct);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const updateProduct = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const productId = req.params.id;
+    const updatedProduct = await Product.findByIdAndUpdate(
+      productId,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const deleteProduct = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const productId = req.params.id;
+    await Product.findByIdAndDelete(productId);
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,20 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import mongoose, { ConnectOptions } from "mongoose";
+import productRouter from "routes/productRoutes";
 
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use("/api", productRouter);
 
 mongoose.connect("mongodb://localhost:27017/ecommerce", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 } as ConnectOptions);
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
