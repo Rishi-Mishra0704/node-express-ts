065333bd789515039e25fd691079ab010484e11b
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-01-05 21:24:29 +0530

    setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("hello");
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import mongoose, { ConnectOptions } from "mongoose";
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb://localhost:27017/ecommerce", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+} as ConnectOptions);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+export default app;
